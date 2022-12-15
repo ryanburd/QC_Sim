b5a90ca2750f1fd8094c4ebf38506e7e8d815147
ryanburd <ryankburdick@gmail.com>
2022-12-15 16:56:17 -0500

    Added Hadamard gate and gate tracker

--- BitsAndGates.py
+++ BitsAndGates.py
@@ -13,22 +13,38 @@ class Circuit:
         qubit_list[:,0] = 1
         self.qubits = qubit_list
         self.cbits = np.zeros([num_cbits,1],dtype=np.int8)
+        self.gates = []
 
     # Pauli-X gate
     def X(self,qubit_index):
         self.qubits[qubit_index,:] = np.dot(np.array([[0,1],[1,0]]),self.qubits[qubit_index])
+        self.gates.append(['X',qubit_index])
         return self
 
     # Pauli-Y gate
     def Y(self,qubit_index):
         self.qubits[qubit_index,:] = np.dot(np.array([[0,-1j],[1j,0]]),self.qubits[qubit_index])
+        self.gates.append(['Y',qubit_index])
         return self
 
     # Pauli-Z gate
     def Z(self,qubit_index):
         self.qubits[qubit_index,:] = np.dot(np.array([[1,0],[0,-1]]),self.qubits[qubit_index])
+        self.gates.append(['Z',qubit_index])
         return self
     
+    # Hadamard gate
     def H(self,qubit_index):
-        self.qubits[qubit_index,:] = np.dot(np.array([[1,1],[1,1]])/np.sqrt(2),self.qubits[qubit_index])
+        self.qubits[qubit_index,:] = np.dot(np.array([[1,1],[1,-1]])/np.sqrt(2),self.qubits[qubit_index])
+        self.gates.append(['H',qubit_index])
         return self
+    
+    # Controlled-X gate (still in progress)
+    def CX(self,c_qubit,t_qubit):
+        tensor = np.tensordot(self.qubits[c_qubit],self.qubits[t_qubit],axes=0).reshape(4,1)
+        CX_tensor = np.dot(np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]]),tensor)
+        return self
+
+test = Circuit(2,2)
+test.CX(0,1)
+print(test.qubits)
