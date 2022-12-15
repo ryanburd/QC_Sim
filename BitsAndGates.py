475e2bdf5741cd03a721414d9a5dd1e51efcd502
ryanburd <ryankburdick@gmail.com>
2022-12-15 12:47:24 -0500

    Added H gate

--- BitsAndGates.py
+++ BitsAndGates.py
@@ -3,25 +3,32 @@
 
 import numpy as np
 
-# Creates an instance of a quantum circuit with a provided number of quantum bits and classical bits
-class Bits:
+# Creates an instance of a quantum circuit with a provided number of quantum bits and classical bits and allows the user
+# to apply qubit gates to the circuit
+class Circuit:
+
+    # Create the provided number of qubits and classical bits upon instance initialization
     def __init__(self,num_qubits,num_cbits):
         qubit_list = np.zeros([num_qubits,2],dtype=np.complex_)
         qubit_list[:,0] = 1
         self.qubits = qubit_list
-        self.cbits = np.zeros([num_cbits,1],dtype=np.complex_)
+        self.cbits = np.zeros([num_cbits,1],dtype=np.int8)
 
-# Pauli-X gate
-def X(self,qubit_index):
-    self.qubits[qubit_index,:] = np.dot(np.array([[0,1],[1,0]]),self.qubits[qubit_index])
-    return self
+    # Pauli-X gate
+    def X(self,qubit_index):
+        self.qubits[qubit_index,:] = np.dot(np.array([[0,1],[1,0]]),self.qubits[qubit_index])
+        return self
 
-# Pauli-Y gate
-def Y(self,qubit_index):
-    self.qubits[qubit_index,:] = np.dot(np.array([[0,-1j],[1j,0]]),self.qubits[qubit_index])
-    return self
+    # Pauli-Y gate
+    def Y(self,qubit_index):
+        self.qubits[qubit_index,:] = np.dot(np.array([[0,-1j],[1j,0]]),self.qubits[qubit_index])
+        return self
 
-# Pauli-Z gate
-def Z(self,qubit_index):
-    self.qubits[qubit_index,:] = np.dot(np.array([[1,0],[0,-1]]),self.qubits[qubit_index])
-    return self
+    # Pauli-Z gate
+    def Z(self,qubit_index):
+        self.qubits[qubit_index,:] = np.dot(np.array([[1,0],[0,-1]]),self.qubits[qubit_index])
+        return self
+    
+    def H(self,qubit_index):
+        self.qubits[qubit_index,:] = np.dot(np.array([[1,1],[1,1]])/np.sqrt(2),self.qubits[qubit_index])
+        return self
