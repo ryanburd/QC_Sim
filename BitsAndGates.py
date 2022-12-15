a70608bb8d166c1c9ca4087fcba17f89870ecb2d
ryanburd <ryankburdick@gmail.com>
2022-12-14 23:50:33 -0500

    Created file

---
+++ BitsAndGates.py
@@ -0,0 +1,27 @@
+## This file allows the user to create a quantum circuit with a provided number of qubits and classical bits.
+## Common qubit gates to operate on the quantum circuit are defined.
+
+import numpy as np
+
+# Creates an instance of a quantum circuit with a provided number of quantum bits and classical bits
+class Bits:
+    def __init__(self,num_qubits,num_cbits):
+        qubit_list = np.zeros([num_qubits,2],dtype=np.complex_)
+        qubit_list[:,0] = 1
+        self.qubits = qubit_list
+        self.cbits = np.zeros([num_cbits,1],dtype=np.complex_)
+
+# Pauli-X gate
+def X(self,qubit_index):
+    self.qubits[qubit_index,:] = np.dot(np.array([[0,1],[1,0]]),self.qubits[qubit_index])
+    return self
+
+# Pauli-Y gate
+def Y(self,qubit_index):
+    self.qubits[qubit_index,:] = np.dot(np.array([[0,-1j],[1j,0]]),self.qubits[qubit_index])
+    return self
+
+# Pauli-Z gate
+def Z(self,qubit_index):
+    self.qubits[qubit_index,:] = np.dot(np.array([[1,0],[0,-1]]),self.qubits[qubit_index])
+    return self
