c0fc8e3306c3d405c4abaacf5a29fe28324b3073
ryanburd <ryankburdick@gmail.com>
2023-03-22 20:05:53 -0400

    Added function to create a figure of the circuit

--- BitsAndGates.py
+++ BitsAndGates.py
@@ -2,6 +2,7 @@
 ## Common qubit gates to operate on the quantum circuit are defined.
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Define common matrices used for gate operations.
 Xmatrix = np.array([[0, 1],
@@ -29,6 +30,7 @@ class Circuit:
 
     # Create the provided number of qubits and classical bits upon instance initialization
     def __init__(self, numQubits, numCbits):
+
         # Create a list of qubits, each intialized in the |0> state, [1, 0].
         qubit_list = np.zeros([numQubits, 2], dtype=np.complex_)
         qubit_list[:, 0] = 1
@@ -49,6 +51,7 @@ class Circuit:
 
     # Pauli-X gate
     def X(self,target):
+
         # Create the Kronecker product matrix defining the gate operation. The target qubit gets an X gate, all other qubits get identity.
         kronMatrix = np.array([1])
         for qIndex, qubit in enumerate(self.qubits):
@@ -67,6 +70,7 @@ class Circuit:
 
     # Pauli-Y gate
     def Y(self,target):
+
         # Create the Kronecker product matrix defining the gate operation. The target qubit gets a Y gate, all other qubits get identity.
         kronMatrix = np.array([1])
         for qIndex, qubit in enumerate(self.qubits):
@@ -85,6 +89,7 @@ class Circuit:
 
     # Pauli-Z gate
     def Z(self,target):
+
         # Create the Kronecker product matrix defining the gate operation. The target qubit gets a Z gate, all other qubits get identity.
         kronMatrix = np.array([1])
         for qIndex, qubit in enumerate(self.qubits):
@@ -103,6 +108,7 @@ class Circuit:
     
     # Hadamard gate
     def H(self,target):
+
         # Create the Kronecker product matrix defining the gate operation. The target qubit gets an H gate, all other qubits get identity.
         kronMatrix = np.array([1])
         for qIndex, qubit in enumerate(self.qubits):
@@ -121,6 +127,7 @@ class Circuit:
     
     # Phase gate
     def S(self,target):
+
         # Create the Kronecker product matrix defining the gate operation. The target qubit gets an S gate, all other qubits get identity.
         kronMatrix = np.array([1])
         for qIndex, qubit in enumerate(self.qubits):
@@ -139,6 +146,7 @@ class Circuit:
     
     # pi/8 gate
     def T(self,target):
+
         # Create the Kronecker product matrix defining the gate operation. The target qubit gets a T gate, all other qubits get identity.
         kronMatrix = np.array([1])
         for qIndex, qubit in enumerate(self.qubits):
@@ -158,7 +166,8 @@ class Circuit:
     ## TWO QUBIT GATES ##
 
     # Controlled-X gate
-    def CX(self, control, target):
+    def CX(self, target, control):
+
         # Create the Kronecker product matrix defining the gate operation. First create separate matrices for projecting the control qubit into the 0 or 1 state. For the projection into the 1 state, the target qubit gets an X gate. All other qubits, including the target in the projection into the 0 state for the control, get an identity. Then add the two matrices together.
         kron0Matrix = np.array([1])
         kron1Matrix = np.array([1])
@@ -178,12 +187,13 @@ class Circuit:
         self.state = np.dot(kronMatrix, self.state)
 
         # Append the gate onto the running list of gates in the circuit.
-        self.gates.append(['CX', control, target])
+        self.gates.append(['X', target, 'C', control])
 
         return self
     
     # Controlled-Y gate
-    def CY(self,control,target):
+    def CY(self, target, control):
+
         # Create the Kronecker product matrix defining the gate operation. First create separate matrices for projecting the control qubit into the 0 or 1 state. For the projection into the 1 state, the target qubit gets a Y gate. All other qubits, including the target in the projection into the 0 state for the control, get an identity. Then add the two matrices together.
         kron0Matrix = np.array([1])
         kron1Matrix = np.array([1])
@@ -203,12 +213,13 @@ class Circuit:
         self.state = np.dot(kronMatrix, self.state)
 
         # Append the gate onto the running list of gates in the circuit.
-        self.gates.append(['CY', control, target])
+        self.gates.append(['Y', target, 'C', control])
 
         return self
     
     # Controlled-Z gate
-    def CZ(self,control,target):
+    def CZ(self, target, control):
+
         # Create the Kronecker product matrix defining the gate operation. First create separate matrices for projecting the control qubit into the 0 or 1 state. For the projection into the 1 state, the target qubit gets a Z gate. All other qubits, including the target in the projection into the 0 state for the control, get an identity. Then add the two matrices together.
         kron0Matrix = np.array([1])
         kron1Matrix = np.array([1])
@@ -228,12 +239,13 @@ class Circuit:
         self.state = np.dot(kronMatrix, self.state)
 
         # Append the gate onto the running list of gates in the circuit.
-        self.gates.append(['CZ', control, target])
+        self.gates.append(['Z', target, 'C', control])
 
         return self
     
     # SWAP gate
-    def SWAP(self,target1,target2):
+    def SWAP(self, target1, target2):
+
         # Create the Kronecker product matrix defining the gate operation. First create 3 separate matrices for the two target qubits to receive X, Y, and Z gates together. All other qubits get an identity. Then add the 3 matrices along with an identity and divide the sum by 2.
         kronXMatrix = np.array([1])
         kronYMatrix = np.array([1])
@@ -253,14 +265,15 @@ class Circuit:
         self.state = np.dot(kronMatrix, self.state)
 
         # Append the gate onto the running list of gates in the circuit.
-        self.gates.append(['SWAP', target1, target2])
+        self.gates.append(['SWAP', target1, 'SWAP', target2])
 
         return self
     
     ## THREE QUBIT GATES ##
 
     # Toffoli gate
-    def Toff(self,control1,control2,target):
+    def Toff(self, target, control1, control2):
+
         # Create the Kronecker product matrix defining the gate operation. First create 4 separate matrices for the 4 possible projection combos of the 2 control qubits. Apply the appropriate projection matrix for each control qubit for each of the 4 matrices. The target only gets an X gate when both controls are projected into the 1 state; the target gets an identity otherwise. All other qubits get an identity. Then add the 4 matrices together.
         kron00Matrix = np.array([1])
         kron01Matrix = np.array([1])
@@ -293,7 +306,7 @@ class Circuit:
         self.state = np.dot(kronMatrix, self.state)
 
         # Append the gate onto the running list of gates in the circuit.
-        self.gates.append(['Toff', control1, control2, target])
+        self.gates.append(['X', target, 'C', control1, 'C', control2])
 
         return self
     
@@ -301,6 +314,7 @@ class Circuit:
 
     # Measure a qubit and store the result in a classical bit. This is a measurement in the computational basis (projection into the 0 or 1 state).
     def measure(self, target, cbit):
+
         # Create 2 Kronecker product matrices for the projection of the target qubit into the 0 or 1 state. All other qubits get an identity.
         kron0Matrix = np.array([1])
         kron1Matrix = np.array([1])
@@ -330,14 +344,74 @@ class Circuit:
         self.gates.append(['M', target, cbit])
 
         return self
+    
+    # Create a figure of the circuit.
+    def display_circuit(self):
+
+        # Create the figure and set some style parameters.
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        plt.rcParams.update({'font.size': 20})
+        numQubits = len(self.qubits)
+        numCbits = len(self.cbits)
+        ax.set(xlim=(0, 5), ylim=(-1*(numQubits+numCbits), 1))
+        ax.set_axis_off()
+
+        # Display the qubit labels and a horizontal line to represent the wire for each qubit's circuit.
+        numGates = len(self.gates)
+        bitLabelPosition = 0.5
+        for qIndex, qubit in enumerate(self.qubits):
+            ax.annotate('$Q_%s$'%qIndex, xy=(bitLabelPosition, -1*qIndex), va='center', ha='center', bbox=dict(boxstyle='square', facecolor='white', edgecolor='none'))
+            ax.plot([bitLabelPosition, numGates], [-1*qIndex, -1*qIndex], color='black')
+        
+        # Display the classical bit labels and a double line to represent their wires. "offset" creates a small spacing between the two plotted lines for each bit to give the double line visual.
+        offset = 0.05
+        for cIndex, cbit in enumerate(self.cbits):
+            ax.annotate('$C_%s$'%cIndex, xy=(bitLabelPosition, -1*(cIndex+numQubits)), va='center', ha='center', bbox=dict(boxstyle='square', facecolor='white', edgecolor='none'))
+            ax.plot([bitLabelPosition, numGates], [-1*(cIndex+numQubits)+offset, -1*(cIndex+numQubits)+offset], color='black')
+            ax.plot([bitLabelPosition, numGates], [-1*(cIndex+numQubits)-offset, -1*(cIndex+numQubits)-offset], color='black')
+
+        # Display each gate applied to the circuit.
+        for gIndex, gate in enumerate(self.gates):
+            # For plotting, the negative of the qubit number will be used for the gate's y value so that qubit 0 can be at the top with additional qubits descending.
+            target = -1*gate[1]
+            gateType = gate[0]
+            gateNumber = gIndex+1
+
+            # For multi-qubit gates, first plot the control gates. This puts the line connecting the control to the target gate behind the target gate (only needed for visual appeal).
+            if len(gate) > 2:
+                for index in range(int(len(gate) / 2)):
+                    # Each gate function returns the target first, so skip the first gate element.
+                    if index == 0:
+                        continue
+                    # Style control gates with a filled dot and connect it with a line to the target gate position.
+                    if gate[index*2] == 'C':
+                        control = -1*gate[index*2+1]
+                        ax.annotate('•', xy=(gateNumber, target), xytext=(gateNumber, control), va='center', ha='center', bbox=dict(boxstyle='Circle, pad=0', facecolor='black'), arrowprops=dict(arrowstyle="-"))
+                    # Style SWAP gates with an X and a line to the swapped qubit's position.
+                    elif gate[index*2] == 'SWAP':
+                        control = -1*gate[index*2+1]
+                        ax.annotate('X', xy=(gateNumber, target), xytext=(gateNumber, control), va='center', ha='center', arrowprops=dict(arrowstyle="-"))
+            
+            # If the target gate is a SWAP, style it with an X.
+            if gateType == 'SWAP':
+                ax.annotate('X', xy=(gateNumber, target), va='center', ha='center')
+            # All other target gates are styled with their gate label in a box.
+            else:
+                ax.annotate(gateType, xy=(gateNumber, target), va='center', ha='center', bbox=dict(boxstyle='square', fc='white'))
+
+        plt.show()
+
+        return
 
 def main():
-    test = Circuit(3, 3)
+    numQubits = 3
+    test = Circuit(numQubits, numQubits)
+    test.X(0)
     test.H(1)
-    print(test.state)
-    test.measure(1, 1)
-    print(test.state)
-    print(test.cbits)
+    test.SWAP(0, 1)
+    test.Toff(1, 0, 2)
+    test.display_circuit()
 
 if __name__ == "__main__":
     main()
